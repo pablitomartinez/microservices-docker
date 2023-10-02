de079899a71b7b4726d03147b2e2d84d11bd7080
pablitomartinez <86635786+pablitomartinez@users.noreply.github.com>
2023-10-02 08:51:17 -0300

    add docker compose.yml

--- characters/src/data/index.js
+++ characters/src/data/index.js
@@ -6,8 +6,8 @@ const url = 'http://database:8004/Character'
 module.exports = {
     list : async () => {
         const { data } = await axios.get(url) 
-        console.log(data);
-        return character
+        // console.log(data);
+        return data
     },
     create: async()=>{
         throw Error('Hay un error al crear el personaje')
--- planets/src/data/index.js
+++ planets/src/data/index.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 
 module.exports = {
     list : async ()=>{
-        const { data } = axios.get('http://database:8004/Planet');
+        const { data } = await axios.get('http://database:8004/Planet');
         return data
     }
 }
